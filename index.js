6c2df439475a4402932336220034a46dd41139ae
Anonymity1234 <latazlimnafsak@gmail.com>
2022-12-04 14:14:01 -0500

    vercel connection

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes')
 const employeeRoutes = require('./routes/employeeRoutes')
 
-// const cors = require('cors')
+const cors = require('cors')
 
 const DB_URL = "mongodb+srv://kafabnu101:65qX6iKL2ENMmQj@cluster0.xlocajv.mongodb.net/comp3123_assigment1?retryWrites=true&w=majority"
 const PORT_NUM = process.env.PORT || 8089
 const app = express();
+// app.options('*', cors()) // include before other routes
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use("/api", userRoutes)
 app.use("/api/emp", employeeRoutes)
-// app.use(cors())
+app.use(cors())
 
 mongoose.connect(DB_URL, {
     useNewUrlParser: true,
