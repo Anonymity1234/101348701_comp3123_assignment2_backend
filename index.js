cc62cd56c5a475668c78566495888174f35c427e
Anonymity1234 <latazlimnafsak@gmail.com>
2022-12-04 14:57:54 -0500

    testasdf

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const cors = require('cors')
 const DB_URL = "mongodb+srv://kafabnu101:65qX6iKL2ENMmQj@cluster0.xlocajv.mongodb.net/comp3123_assigment1?retryWrites=true&w=majority"
 const PORT_NUM = process.env.PORT || 8089
 const app = express();
-app.options('*', cors()) // include before other routes
 app.use(cors())
+app.options('*', cors()) // include before other routes
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use("/api", userRoutes)
